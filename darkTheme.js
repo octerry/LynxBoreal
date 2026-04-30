6af5e8ddf03b9c99e4509c419fc6cae740d4273c
Octerry <taaniyu@gmail.com>
2026-04-30 17:21:10 +0200

    Avancement du darkmode

--- darkTheme.js
+++ darkTheme.js
@@ -1,1 +1,21 @@
+// On récup ce dont on a besoin
 let all = document.querySelectorAll();
+let darkButton = document.getElementById("dark_theme_button");
+
+// On défini la variable de darkmode et on lui mets la préférence du navigateur
+let darkModeEnabled = false;
+darkModeEnabled = window.matchMedia('(prefers-color-scheme: dark)').matches;
+
+darkButton.addEventListener("click", ()=>{
+    darkModeEnabled = !darkModeEnabled;
+
+    if (darkModeEnabled) {
+        all.forEach(element => {
+            element.classList.add("dark")
+        });
+    } else {
+        all.forEach(element => {
+            element.classList.remove("dark")
+        });
+    }
+})
