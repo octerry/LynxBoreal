54f8db0dee58d630de8188768b91f84880edcf63
Octerry <158333744+octerry@users.noreply.github.com>
2026-05-07 14:46:45 +0200

    end : darkmode

--- darkTheme.js
+++ darkTheme.js
@@ -1,10 +1,16 @@
 // On récup ce dont on a besoin
-let all = document.querySelectorAll();
+let all = document.querySelectorAll("*");
 let darkButton = document.getElementById("dark_theme_button");
 
 // On défini la variable de darkmode et on lui mets la préférence du navigateur
 let darkModeEnabled = false;
 darkModeEnabled = window.matchMedia('(prefers-color-scheme: dark)').matches;
+
+if (darkModeEnabled) {
+    all.forEach(element => {
+        element.classList.add("dark")
+    });
+}
 
 darkButton.addEventListener("click", ()=>{
     darkModeEnabled = !darkModeEnabled;
